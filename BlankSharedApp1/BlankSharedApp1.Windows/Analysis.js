f2c5b96fe11bb43bc7f6a1cbad9317ab7c9ceb17
Volodymyr Bondariev <vladmikhbond@hotmail.com>
2016-12-28 21:48:06 +0200

    The tests are objects

--- BlankSharedApp1/BlankSharedApp1.Windows/Analysis.js
+++ BlankSharedApp1/BlankSharedApp1.Windows/Analysis.js
@@ -1,12 +1,24 @@
 ﻿//
+//
+function Test(name, success, fail, fun) {
+    this.name = name;
+    this.success = success;
+    this.fail = fail;
+    this.fun = fun;
+}
 
+Test.prototype.do = function () {
+    var isValid = this.fun();
+    return this.fun() ? this.success : this.fail;
+}
+
+// 
+//
 function Analysis(scetch) {
     var L = Trace.prototype.MIN_POINT_COUNT - 1;
     var pi = Math.PI;
     var h = scetch.height;
     var w = scetch.width;
-
-
 
     // Quantors
 
@@ -27,88 +39,91 @@ function Analysis(scetch) {
         return count == 2;
     }
 
-
     // Tests
 
     var tests = [];
 
-    var f = function () {
-        return Exists(e => {
-            return e.type == 'line' && 80 < e.alpha && e.alpha < 110 && e.length > h / 2;
-        });
-    };
-    f.comment = 'длинная вертикальная линия'; f.t = [1, 4, 7]; f.f = [0, 2, 3, 5, 6, 8]; 
-    tests[0] = f;
+    tests[0] = new Test('длинная вертикальная линия', [1, 4, 7], [0, 2, 3, 5, 6, 8],
+        function () {
+            return Exists(e => { return e.type == 'line' && 80 < e.alpha && e.alpha < 110 && e.length > h / 2; });
+        }
+    );
 
+    tests[1] = new Test('длинная диагональная линия или дуга L', [7], null,
+        function () {
+            return Exists(e => { return (e.type == 'line' || e.type == 'arc' && e.arc == 'L') && 110 < e.alpha && e.alpha < 135 && e.length > h / 2; });
+        }
+    );
 
-    tests[7] = function () {
-        return Exists(e => {
-            return (e.type == 'line' || e.type == 'arc' && e.arc == 'L') && 110 < e.alpha && e.alpha < 135 && e.length > h / 2;
-        }) && [7];
-    };
-    tests[7].comment = 'длинная диагональная линия или дуга L';
+    tests[2] = new Test('горизонтальная линия в середине', [4], null,
+        function () {
+            return Exists(e => {
+                if (e.type != 'line') return false;
+                var y = (e.p1.y + e.p2.y) / 2;
+                return (e.alpha < 10 || e.alpha > 170) && h / 5 < y && y < 4 * h / 5;
+            });
+        }
+    );
 
-    tests[5] = function () {
-        return Exists(e => {
-            if (e.type != 'line') return false;
-            var y = (e.p1.y + e.p2.y) / 2;
-            return (e.alpha < 10 || e.alpha > 170) && h / 5 < y && y < 4 * h / 5;
-        }) && [4];
-    }
-    tests[5].comment = 'горизонтальная линия в середине';
+    tests[3] = new Test('дуга L в верхней половине', [4], null,
+        function () {
+            return Exists(e => {
+                if (e.type != 'arc') return false;
+                var y = e.center.y;
+                return e.arc == 'L' && 15 < e.alpha && e.alpha < 75 && y < h / 2;
+            });
+        }
+    );
 
-    tests[6] = function () {
-        return Exists(e => {
-            if (e.type != 'arc') return false;
-            var y = e.center.y;
-            return e.arc == 'L' && 15 < e.alpha && e.alpha < 75 && y < h / 2;
-        }) && [4];
-    }
-    tests[6].comment = 'дуга L в верхней половине';
+    tests[4] = new Test('дуга R в верхней половине', [2,3], null,
+        function () {
+            return Exists(e => {
+                if (e.type != 'arc') return false;
+                var y = e.center.y;
+                return e.arc == 'R' && y < h / 2;
+            });
+        }
+    );
 
-    tests[1] = function () {
-        return Exists(e => {
-            if (e.type != 'arc') return false;
-            var y = e.center.y;
-            return e.arc == 'R' && y < h / 2;
-        }) && [2,3];
-    }
-    tests[1].comment = 'дуга R в верхней половине';
+    tests[5] = new Test('дуга R в нижней половине', [3, 5], null,
+        function () {
+            return Exists(e => {
+                if (e.type != 'arc') return false;
+                var y = e.center.y;
+                return e.arc == 'R' && h / 2 < y;
+            });
+        }
+    );
 
-    tests[2] = function () {
-        return Exists(e => {
-            if (e.type != 'arc') return false;
-            var y = e.center.y;
-            return e.arc == 'R' && h / 2 < y;
-        }) && [3,5];
-    }
-    tests[2].comment = 'дуга R в нижней половине';
+    tests[6] = new Test('горизонтальная линия или дуга вверху', [5, 7], null,
+        function () {
+            return Exists(e => {
+                if (e.type == 'loop') return false;
+                var y = (e.p1.y + e.p2.y) / 2;
+                return (e.alpha < 10 || e.alpha > 170) && y < h / 5;
+            });
+        }
+    );
 
-    tests[3] = function () {
-        return Exists(e => {
-            if (e.type == 'loop') return false;
-            var y = (e.p1.y + e.p2.y) / 2;
-            return (e.alpha < 10 || e.alpha > 170) && y < h / 5;
-        }) && [5, 7];
-    }
-    tests[3].comment = 'горизонтальная линия или дуга вверху';
+    tests[7] = new Test('горизонтальная линия или дуга вверху', [5, 7], [0, 1, 2, 3, 4, 6, 8, 9],
+        function () {
+            return Exists(e => {
+                if (e.type == 'loop') return false;
+                var y = (e.p1.y + e.p2.y) / 2;
+                return (e.alpha < 10 || e.alpha > 170) && y < h / 5;
+            });
+        }
+    );
 
-    tests[8] = function () {
-        return !tests[3]() && [0, 1, 2, 3, 4, 6, 8, 9];
-    }
-    tests[8].comment = 'НЕТ горизонтальной линии или дуги вверху';
-
-    tests[4] = function () {
-        return Exists(e => {
-            if (e.type != 'line') return false;
-            var y = (e.p1.y + e.p2.y) / 2;
-
-            return (e.alpha < 10 || e.alpha > 170) && y > 4 * h / 5;
-        }) && [2];
-    }
-    tests[4].comment = 'горизонтальная линия внизу';
-
-
+    tests[8] = new Test('горизонтальная линия внизу', [2], null,
+        function () {
+            return Exists(e => {
+                if (e.type != 'line') return false;
+                var y = (e.p1.y + e.p2.y) / 2;
+                return (e.alpha < 10 || e.alpha > 170) && y > 4 * h / 5;
+            });
+        }
+    );
 
 
     this.doTests = function () {
@@ -116,14 +131,13 @@ function Analysis(scetch) {
         var report = "";
 
         // set intersection
-        for (i in [0]) {
-            var res = tests[i]();
-            if (res) {
-                report += "test " + i + "  " + tests[i].comment + "\n";
-                result = intersect(result, tests[i].t);
-            } else {
-                result = intersect(result, tests[i].f);
-            }
+        for (i in tests) {
+            var set = tests[i].do();
+            if (set) {
+                result = intersect(result, set);
+            }                
+            report += "test " + i + "  " + tests[i].name + "  " + set + "\n";
+
         }
         report += "RESULT: " + result + "\n";
 
